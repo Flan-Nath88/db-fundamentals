d40fd1270d3cddf0ebb2d9c17d4cc7b5d1cf4fce
mrmorgan-i <misraelkelly@gmail.com>
2025-10-28 18:19:47 -0500

    add conditional rendering of signin/signout

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,15 +1,16 @@
 import SignIn from "./signin-button"
 import Link from "next/link"
 import SignOut from "./signout-button"
+import { auth } from "@/auth"
 
-export default function NavBar() {
+export default async function NavBar() {
+    const session = await auth();
     return (
         <nav className="bg-white dark:bg-gray-800 shadow-lg">
             <div className="container mx-auto px-4 py-4">
                 <div className="flex items-center justify-between">
                     <Link href="/" className="text-2xl font-bold">Task Master</Link>
-                    <SignIn />
-                    <SignOut />
+                    {session ? <SignOut /> : <SignIn />}
                 </div>
             </div>
         </nav>
